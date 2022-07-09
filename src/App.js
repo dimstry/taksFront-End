3530b16704779ec9d2ded07804d0eb4310b8e58b
dimstry <dimastriana03@gmail.com>
2022-07-09 07:44:32 +0700

    2 soal complite

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import './App.css';
 import { useEffect, useState } from 'react'
 
 function App() {
+  /* variabel sementara */
   const [ datas, setDatas ] = useState([])
   
+  /* function mengambil data api nya */
   const getBerrys = async () => {
     const berrys = await fetch("https://pokeapi.co/api/v2/berry/")
     const value = await berrys.json()
-    const result = value.results.map(data => {
+    /* manipulasi data api supaya sama dengan select react nya */
+    let result = value.results.map(data => {
       return {
         label: data.name,
         value: data.label
       }
     })
+    /* Membuat supaya menjadi urutan ascending */
+    result = result.sort((a,b) => a.label.localeCompare(b.label))
+    /* memasukan hasil ke variabel sementara / useState */
     setDatas(result)
   }
   
+  
+  /* ketika pertama kali di reander */
   useEffect(() => {
+    /* jalankan function getBerrys */
     getBerrys()
   }, [])
   return (
